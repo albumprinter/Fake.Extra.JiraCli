16dc53bc9d48adccd854cfd578149eccf5b0c3d7
Denys Zhuravel <denys.zhuravel@albelli.com>
2017-11-08 15:08:48 +0100

    WIP

--- JiraCli.fs
+++ JiraCli.fs
@@ -1,9 +1,13 @@
 module Fake.JiraCliHelper
 
+open System
+
 open System.Text.RegularExpressions
 
 open Fake
 open Fake.Git.CommandHelper
+
+open Microsoft.FSharp.Core
 
 
 type JiraCliConfig = {
@@ -12,21 +16,25 @@ type JiraCliConfig = {
             password : string
         }
 
-type Either<'a, 'b> =
- Success of 'a
- | Failure of 'b
+type private EitherBuilder<'a, 'b>() =
+    member x.Bind(m,f) =
+             match m with
+                | Ok s -> f s
+                | Error _ -> m
+    member x.Return a = Ok a
+    member x.ReturnFrom (o : Result<'a, 'b>) = o
 
-type private ErrorFallBackBuilder() =
-    member x.Bind(v,f) = 
-                match v with
-                Success a -> (f v) |> Success
-                | Failure b -> Failure b
-    member x.Return a = Success a
-    member x.ReturnFrom o = o
+    member x.Zero() = Error ""
 
-let private errorFlow = ErrorFallBackBuilder()
+let private errorFlow = EitherBuilder()    
 
-let jiraCli arg config =
+type JiraCliCommands =
+    CustomExec of string
+    | AssignVersion of project:string * version:string * issues:string list
+    | CreateVersion of project:string * version:string
+    | ReleaseVersion of project:string * version:string
+
+let jiraCliExec config arg =
     let jiraCliExe = findToolInSubPath "JiraCli.exe" "packages"
     ProcessHelper.enableProcessTracing <- false
     let fullArgs = sprintf "-user \"%s\" -pw \"%s\" -url \"%s\" %s" config.userName config.password config.url arg
@@ -36,15 +44,40 @@ let jiraCli arg config =
     if (not success) then failwith "failed to run JiraCli"
     ProcessHelper.enableProcessTracing <- true
 
+let private getCmdFromJiraCliCommand = function
+    CustomExec arg -> arg
+    | AssignVersion (project,version,issues)
+        -> sprintf "-action AssignVersion -project \"%s\" -version \"%s\" -issues \"%s\"" 
+            project
+            version 
+            (issues |> String.concat ",")
+    | CreateVersion (project, version)
+        -> sprintf "-action CreateVersion -project \"%s\" -version \"%s\"" project version
+    | ReleaseVersion (project, version)
+        -> sprintf "-action ReleaseVersion -project \"%s\" -version \"%s\"" project version
+
+
+let jiraCli (cfg : JiraCliConfig->JiraCliConfig) cmd =
+    let configuration =
+        {
+                url = ""
+                userName = ""
+                password = ""
+        } |> cfg
+    let actualCommand = cmd |> getCmdFromJiraCliCommand
+
+    cmd
+    |> getCmdFromJiraCliCommand
+    |> jiraCliExec configuration
 
 let runGitCommandOpt cmd = 
     let success, msg, err =
         cmd
         |> runGitCommand currentDirectory
     if success then
-        Success msg
+        Ok msg
     else
-        Failure err        
+        Error err        
 
 let parseJiraIdsFromCommits commits =
     let regex = "[a-zA-Z]+-\\d+"
@@ -66,5 +99,5 @@ let getCommitsSinceLastTag() =
         return! runGitCommandOpt (sprintf "log %s..HEAD --pretty=format:\"%%s\"" lastTagHead)
     }
     |> fun x -> match x with
-                Success a -> a
-                | Failure _ -> failwith "Unable to retrieve git commits since last tag"
+                Ok a -> a
+                | Error _ -> failwith "Unable to retrieve git commits since last tag"
