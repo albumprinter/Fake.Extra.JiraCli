5e02a4e81bf728161ed6a594e9e3d88719456acb
Denys Zhuravel <denys.zhuravel@albelli.com>
2017-11-08 15:35:47 +0100

    magicallyPublishJiraRelease

--- JiraCli.fs
+++ JiraCli.fs
@@ -16,17 +16,39 @@ type JiraCliConfig = {
             password : string
         }
 
-type private EitherBuilder<'a, 'b>() =
-    member x.Bind(m,f) =
-             match m with
-                | Ok s -> f s
-                | Error _ -> m
-    member x.Return a = Ok a
-    member x.ReturnFrom (o : Result<'a, 'b>) = o
+    module Internal = 
+        type private EitherBuilder<'a, 'b>() =
+            member __.Bind(m,f) =
+                     match m with
+                        | Ok s -> f s
+                        | Error _ -> m
+            member __.Return a = Ok a
+            member __.ReturnFrom (o : Result<'a, 'b>) = o
 
-    member x.Zero() = Error ""
+            member __.Zero() = Error ""
 
-let private errorFlow = EitherBuilder()    
+        let private errorFlow = EitherBuilder()
+
+        let private runGitCommandOpt cmd = 
+            let success, msg, err =
+                cmd
+                |> runGitCommand currentDirectory
+            if success then
+                Ok msg
+            else
+                Error err
+
+        let getCommitsSinceLastTag() =
+            errorFlow {
+                let! lastTag = runGitCommandOpt "describe --tags --abbrev=0 HEAD^"
+                return! lastTag
+                        |> Seq.head
+                        |> sprintf "log %s..HEAD --pretty=format:\"%%s\""
+                        |> runGitCommandOpt
+            }
+            |> fun x -> match x with
+                        Ok a -> a
+                        | Error _ -> failwith "Unable to retrieve git commits since last tag"
 
 type JiraCliCommands =
     CustomExec of string
@@ -34,10 +56,24 @@ type JiraCliCommands =
     | CreateVersion of project:string * version:string
     | ReleaseVersion of project:string * version:string
 
+let private getDefaultParametersForCommand = function
+    CustomExec arg -> arg
+    | AssignVersion (project,version,issues)
+        -> sprintf "-action AssignVersion -project \"%s\" -version \"%s\" -issues \"%s\"" 
+            project
+            version 
+            (issues |> String.concat ",")
+    | CreateVersion (project, version)
+        -> sprintf "-action CreateVersion -project \"%s\" -version \"%s\"" project version
+    | ReleaseVersion (project, version)
+        -> sprintf "-action ReleaseVersion -project \"%s\" -version \"%s\"" project version
+
+
 let private jiraCliExec config arg =
     let jiraCliExe = findToolInSubPath "JiraCli.exe" "packages"
     ProcessHelper.enableProcessTracing <- false
     let fullArgs = sprintf "-user \"%s\" -pw \"%s\" -url \"%s\" %s" config.userName config.password config.url arg
+    
     let success = execProcess 
                     (fun p -> p.FileName <- jiraCliExe; p.Arguments <- fullArgs)
                     (TimeSpan.FromMinutes 2.)
@@ -70,14 +106,7 @@ let jiraCli (cfg : JiraCliConfig->JiraCliConfig) cmd =
     |> getCmdFromJiraCliCommand
     |> jiraCliExec configuration
 
-let runGitCommandOpt cmd = 
-    let success, msg, err =
-        cmd
-        |> runGitCommand currentDirectory
-    if success then
-        Ok msg
-    else
-        Error err        
+
 
 let parseJiraIdsFromCommits commits =
     let regex = "[a-zA-Z]+-\\d+"
@@ -92,12 +121,20 @@ let parseJiraIdsFromCommits commits =
     |> String.concat " "
     |> matches regex
 
-let getCommitsSinceLastTag() =
-    errorFlow {
-        let! lastTag = runGitCommandOpt "describe --tags --abbrev=0 HEAD^"
-        let lastTagHead = lastTag |> Seq.head
-        return! runGitCommandOpt (sprintf "log %s..HEAD --pretty=format:\"%%s\"" lastTagHead)
-    }
-    |> fun x -> match x with
-                Ok a -> a
-                | Error _ -> failwith "Unable to retrieve git commits since last tag"
+let magicallyPublishJiraRelease projectName version cfg=
+    
+    let assignCommitsToVersion version commits = 
+        AssignVersion (projectName, version, commits)
+        |> jiraCli cfg
+    
+    CreateVersion (projectName, version)
+    |> jiraCli cfg
+    
+    Internal.getCommitsSinceLastTag()
+    |> parseJiraIdsFromCommits
+    |> Seq.filter (fun x -> x.Contains(projectName)) // filterNonProjectIds
+    |> List.ofSeq
+    |> assignCommitsToVersion version
+
+    ReleaseVersion (projectName, version)
+    |> jiraCli cfg 
